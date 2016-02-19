42559fafe071851a5113f67c37a4c00cd2d9ca98
Aria Stewart <aredridel@dinhe.net>
2016-02-19 08:30:14 -0500

    Remove all the cruft

--- render.js
+++
@@ -1,5 +0,0 @@
-// A trivial 'render' function for my component^Wapplication
-module.exports = function render(formatter) {
-    document.querySelector('p').innerText =
-        formatter.format("bag", { items: 2});
-}
